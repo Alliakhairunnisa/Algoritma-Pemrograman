e8bfd284cad2e03bfe58bc1c45da28bc4131c1da
Alliakhairunnisa <khairunnisaallia42@gmail.com>
2024-09-29 15:27:18 +0700

    Alliakhairunnisa.cpp

--- Alliakhairunnisa.cpp
+++ Alliakhairunnisa.cpp
@@ -10,28 +10,29 @@ int main()
   float penjualan[10],komisi[10],ttlkomisi=0.0;
   int i,jlhdata=10;
   
-  cout<<"KOMISI SALESMAN PT.MURAH HATI"<<endl;
-  cout<<"❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎❦︎"<<endl;
+  cout<<" ===================="<<endl;
+  cout<<"| PT.MURAH HATI |"<<endl;
+  cout<<" ====================\n"<<endl;
   cout<<setprecision(12);
   for(int i=0;i<jlhdata;i++) 
   {
     cout<<"Data Ke-"<<i+1<<endl;
-    cout<<"===================================="<<endl;
     cout<<"Nama Salesman : ";
     cin>>nama[i];
     cout<<"Jumlah Penjualan : ";
     cin>>penjualan [i];
-    (penjualan[i]<=500000);
+    if(penjualan[i]<=500000) {
     komisi[i]=0.10*penjualan[i];
+    } else {
     komisi[i]=0.10*500000+0.15*(penjualan[i]-500000);
+    }
     ttlkomisi+=komisi[i];
-    cout<<"Komisi : "<<komisi[i]<<endl;
-    cout<<"===================================="<<endl;
+    cout<<"Komisi : "<<komisi[i]<<"\n"<<endl;
   }
-  cout<<"----------------------------------------------------------------------"<<endl;
-  cout<<"Total Komisi yang dibayarkan PT.MURAH HATI : "<<ttlkomisi<<endl;
-  cout<<"----------------------------------------------------------------------"<<endl;
+  cout<<"=======≠=============================================================="<<endl;
+  cout<<"\n Total Komisi yang dibayarkan PT.MURAH HATI : \n"<<ttlkomisi<<endl;
+  cout<<"======================================================================"<<endl;
     
     return 0;
-
+  
 } 
